1991ddab1a9d6f863d41bc518f4540ba014ff3ac
celines <celina.alsina@gmail.com>
2023-05-23 15:18:51 +0200

    Some styles and date format

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,15 +1,22 @@
 import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
-
+import Head from 'next/head';
+import Date from '../../components/date';
+import utilStyles from '../../styles/utils.module.css';
 
 export default function Post({ postData }) {
     return (
         <Layout>
-            {postData.title}
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            <h1 className={utilStyles.headingXl}>{postData.title}</h1>
             <br />
             {postData.id}
             <br />
-            {postData.date}
+            <div className={utilStyles.lightText}>
+                <Date dateString={postData.date} />
+            </div>
             <br />
             <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
         </Layout>
